218141eccef5bafeb5a1034b4c3d559bad7a5d8b
Jaazbe <jaazbeh@gmail.com>
2016-11-29 14:02:53 -0800

    First Edit
    Worked on the Tribute web page. It is not complete.
    Design and implementation is half way through.

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/Jaazbe/pen/XNevvB.
+
+ Black is using SCSS for styling. Other modifications will should be added here.
---
+++ js/index.js
@@ -0,0 +1,10 @@
+$("h1").addClass("text-center");
+
+var im1 = document.createElement("img");
+
+$("#im1").attr({
+	src:"http://visitwindsoressex.com/wp-content/uploads/2015/10/Poppy-2.png",
+	alt : "rememberance day"
+});
+
+$("footer").children().css("font-size","small");
